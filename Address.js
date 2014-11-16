a5a0719f0c9eb51ff63d2fedf6692bb62a78f434
danharel <cheezzywizz@gmail.com>
2014-11-16 09:57:53 -0500

    Finished connecting front end to back end

--- Address.js
+++ Address.js
@@ -31,15 +31,19 @@ function getWeather(Latias, Latios){
 	var URL = "http://api.openweathermap.org/data/2.5/weather?lat=" + Latias + "&lon=" + Latios + "139&APPID=35e9b3fc58da0908910ee74db4a29357";
     //console.log(URL);
 	$.getJSON(URL, function(data, status) {	    
-	    processWeather(data);
+	    getPreferences(data, processWeather);
    });
 }
 
-function processWeather(data) {
-    var pref = '{"user": { "username":"danharel", "preferences":[{"heattolerance":-1}, {"coldtolerance":1 }]}}'
-    pref = JSON.parse(pref);
-    var heat = pref.user.preferences[0].heattolerance;
-    var cold = pref.user.preferences[1].coldtolerance;
+function processWeather(data, pref) {
+    /*var pref = {
+        heattolerance:-1, 
+        coldtolerance:1 
+    }*/
+    //pref = JSON.parse(pref);
+    //var pref = getPreferences();
+    var heat = pref.heatTolerance;
+    var cold = pref.chillTolerance;
     var location = data.name;
     var temp = (data.main.temp - 273.15)*1.8 + 32;
     var calctemp = temp;
@@ -77,4 +81,52 @@ function processWeather(data) {
 	console.log("It's hot. You'll want just a t-shirt.");
     else 
 	console.log("It's very hot. You'll want a tank top.");
+}
+
+var QueryString = function () {
+    // This function is anonymous, is executed immediately and 
+    // the return value is assigned to QueryString!
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+      var pair = vars[i].split("=");
+        // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+          query_string[pair[0]] = pair[1];
+        // If second entry with this name
+      } else if (typeof query_string[pair[0]] === "string") {
+        var arr = [ query_string[pair[0]], pair[1] ];
+        query_string[pair[0]] = arr;
+        // If third or later entry with this name
+      } else {
+        query_string[pair[0]].push(pair[1]);
+      }
+    } 
+    return query_string;
+  } ();
+
+function getPreferences(weatherData, callback) {
+    var server_url = "https://blooming-cliffs-6326.herokuapp.com/"
+    var username = QueryString.username;
+
+    var data = {
+        username: username
+    }
+
+    $.post(server_url+'getUserPreferences', data)
+    .done(function(res) {
+        //console.log(res);
+      var status = res.status;
+      if (res.status === 'success') {
+        //console.log(res.user.preferences);
+        callback(weatherData, res.user.preferences);
+      }
+      else {
+        alert(res.message);
+      }
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      alert("Text: " + textStatus + "\nthrown: " + errorThrown);
+    });
 }
