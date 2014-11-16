6ac83bfb8e316e076dfff6537c3f0fb089e10633
danharel <cheezzywizz@gmail.com>
2014-11-16 02:41:25 -0500

    Fixed shotty user adding

--- js/signup.js
+++ js/signup.js
@@ -1,16 +1,18 @@
 var url = 'https://blooming-cliffs-6326.herokuapp.com';
 
 function signUp() {
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	$.post(url+'/newUser', {
-		username: username, 
-		password: password
-	})
-	.done(function(res) {
-		alert(res.message);
-	})
-	.fail(function(jqXHR, textStatus, errorThrown ) {
-		alert(textStatus);
-	});
+	if (document.getElementById("matching").value === "true") {
+		var username = document.getElementById("username").value;
+		var password = document.getElementById("password").value;
+		$.post(url+'/newUser', {
+			username: username, 
+			password: password
+		})
+		.done(function(res) {
+			alert(res.message);
+		})
+		.fail(function(jqXHR, textStatus, errorThrown ) {
+			alert(textStatus);
+		});
+	}
 }
