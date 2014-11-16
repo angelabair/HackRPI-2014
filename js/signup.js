e7331878a2318cd6297ad61eae36b822a84d1e0e
danharel <cheezzywizz@gmail.com>
2014-11-16 09:24:43 -0500

    Did I make any changes?'

--- js/signup.js
+++ js/signup.js
@@ -1,19 +1,33 @@
-var url = 'https://blooming-cliffs-6326.herokuapp.com';
+//var url = 'https://blooming-cliffs-6326.herokuapp.com';
+var url = 'https://blooming-cliffs-6326.herokuapp.com/';
 
 function signUp() {
 	if (document.getElementById("matching").value === "true") {
 		var username = document.getElementById("username").value;
 		var password = document.getElementById("password").value;
-		alert("Username: " + username + "\npassword: " + password);
-		$.post(url+'/newUser', {
-			username: username, 
+		var data = {
+			username: username,
 			password: password
-		})
+		};
+		//alert(data);
+		//alert("Username: " + username + "\npassword: " + password);
+		$.post(url+'newUser', data)
 		.done(function(res) {
-			alert("Success Message: " + res.message);
+			if (res.status === 'success')
+				window.location.replace("preferences.html");
+			else
+				alert(res.message);
 		})
-		.fail(function(jqXHR, textStatus, errorThrown ) {
-			alert("Error thrown: " + errorThrown + "; Text status: " + textStatus);
+		.fail(function(jqXHR, textStatus, errorThrown) {
+			alert("Text: " + textStatus + "\nthrown: " + errorThrown);
 		});
 	}
+}
+
+function success(data) {
+	alert("Success!");
+}
+
+function error(jq, textStatus, errorThrown) {
+	alert("Text status: " + textStatus + "\nerrot thrown: " + errorThrown);
 }
