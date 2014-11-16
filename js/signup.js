9d37f3d086d691dfede432588650321b291a1a17
danharel <cheezzywizz@gmail.com>
2014-11-16 02:24:07 -0500

    Refactored I guess?

--- js/signup.js
+++ js/signup.js
@@ -3,12 +3,6 @@ var url = 'https://blooming-cliffs-6326.herokuapp.com';
 function signUp() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
-	var confirm_password = document.getElementById("confirm_password");
-
-	if (password !== confirm_password) {
-		alert("Password #1 and password #2 must match!");
-	}
-
 	$.post(url+'/addUser', {
 		username: username, 
 		password: password
